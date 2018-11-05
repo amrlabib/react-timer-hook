a1e765116800ab37fcb7a723a31f54efaa80ce42
Amr Labib <amr.h.labib@gmail.com>
2018-11-05 21:13:10 +0400

    react app with useTimer customHook

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import useTimer  from './useTimer';
+
+export default function App() {
+  const now = new Date()
+  const after10Days = now.setDate(now.getDate() + 10);
+  const { seconds, minutes, hours, days, startTimer, stopTimer, clearTimer } = useTimer({
+    autoStart: true,
+    expiryTimeStamp: after10Days,
+  });
+
+
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>react-timer-hook Demo</h1>
+      <br/>
+      <br/>
+      <div style={{fontSize: '100px'}}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+      </div>
+      <br/>
+      <br/>
+      <button onClick={startTimer}>Start</button>
+      <br/>
+      <br/>
+      <button onClick={stopTimer}>Stop</button>
+      <br/>
+      <br/>
+      <button onClick={clearTimer}>Clear</button>
+    </div>
+  );
+}
