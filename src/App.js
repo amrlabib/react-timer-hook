e2cd4296e398fb47b5ab97ff7c841c2998bd466b
Amr Labib <amr.h.labib@gmail.com>
2018-11-18 16:14:46 +0400

    minor demo code enhancements

--- src/App.js
+++ src/App.js
@@ -4,28 +4,25 @@ import useTimer  from './useTimer';
 export default function App() {
   const now = new Date()
   const after10Days = now.setDate(now.getDate() + 10);
-  const { seconds, minutes, hours, days, startTimer, stopTimer, resetTimer } = useTimer({
-    autoStart: true,
-    expiryTimestamp: after10Days,
-  });
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    startTimer,
+    stopTimer,
+    resetTimer,
+  } = useTimer({ autoStart: true });
 
 
   return (
     <div style={{textAlign: 'center'}}>
       <h1>react-timer-hook Demo</h1>
-      <br/>
-      <br/>
       <div style={{fontSize: '100px'}}>
         <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
       </div>
-      <br/>
-      <br/>
       <button onClick={startTimer}>Start</button>
-      <br/>
-      <br/>
       <button onClick={stopTimer}>Stop</button>
-      <br/>
-      <br/>
       <button onClick={resetTimer}>Reset</button>
     </div>
   );
