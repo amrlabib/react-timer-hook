b2b09eb585095f56684b6b140856d3cf2e556075
Amr Labib <amr.h.labib@gmail.com>
2019-02-28 02:16:23 +0400

    update isValidExpiryTimestamp validation to expect expiryTimestamp to be required

--- src/useTimer.js
+++ src/useTimer.js
@@ -163,9 +163,9 @@ export function useTimer(settings) {
 
   // Validate expiryTimestamp
   function isValidExpiryTimestamp(expiryTimestamp) {
-    const isValid = expiryTimestamp && (new Date(expiryTimestamp)).getTime() > 0;
-    if(expiryTimestamp && !isValid) {
-      console.warn('react-timer-hook: Invalid expiryTimestamp settings passed', expiryTimestamp);
+    const isValid = (new Date(expiryTimestamp)).getTime() > 0;
+    if(!isValid) {
+      console.warn('react-timer-hook: { useTimer } Invalid expiryTimestamp settings', expiryTimestamp);
     }
     return isValid;
   }
