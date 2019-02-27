2791dbc8d25db6f673e5146b625b43fec4620b89
Amr Labib <amr.h.labib@gmail.com>
2019-02-28 01:55:54 +0400

    use useTimer and useStopwatch inside deprecatedUseTimer and log deprecation warning

--- src/useTimer.js
+++ src/useTimer.js
@@ -1,147 +1,19 @@
 import { useState, useEffect, useRef } from 'react';
 
-export default function useTimer(settings) {
-  const { autoStart, expiryTimestamp, onExpire } = settings || {};
-
-  // Seconds
-  const [seconds, setSeconds] = useState(0);
-  function addSecond() {
-    setSeconds(prevSeconds => {
-      if(prevSeconds === 59) {
-        addMinute();
-        return 0;
-      }
-      return prevSeconds + 1;
-    });
-  }
-
-  // Minutes
-  const [minutes, setMinutes] = useState(0);
-  function addMinute() {
-    setMinutes(prevMinutes => {
-      if (prevMinutes === 59) {
-        addHour();
-        return 0;
-      }
-      return prevMinutes + 1;
-    });
-  }
-
-  // Hours
-  const [hours, setHours] = useState(0);
-  function addHour() {
-    setHours(prevHours => {
-      if (prevHours === 23) {
-        addDay();
-        return 0;
-      }
-      return prevHours + 1;
-    });
-  }
-
-  // Days
-  const [days, setDays] = useState(0);
-  function addDay() {
-    setDays(prevDays => {
-      return prevDays + 1;
-    });
-  }
-
-  // Control functions
-  const intervalRef = useRef();
-  function start(isFunctionTrigger) {
-    if (expiryTimestamp) {
-      isValidExpiryTimestamp(expiryTimestamp) && runCountdownTimer();
-    } else if(autoStart) {
-      runTimer();
-    }
-  }
-
-  function runCountdownTimer() {
-    if(!intervalRef.current) {
-      calculateExpiryDate();
-      intervalRef.current = setInterval(() => calculateExpiryDate(), 1000);
-    }
-  }
-
-  function startTimer() {
-    runTimer();
-  }
-
-  function stopTimer() {
-    if(intervalRef.current) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = undefined;
-    }
-  }
-
-  function runTimer() {
-    if(!intervalRef.current) {
-      intervalRef.current = setInterval(() => addSecond(), 1000);
-    }
-  }
-
-  function resetTimer() {
-    if (intervalRef.current) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = undefined;
-    }
-    setSeconds(0);
-    setMinutes(0);
-    setHours(0);
-    setDays(0);
-  }
-
-  // Timer expiry date calculation
-  function calculateExpiryDate() {
-    var now = new Date().getTime();
-    var distance = expiryTimestamp - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    if(seconds < 0) {
-      resetTimer();
-      isValidOnExpire(onExpire) && onExpire();
-    } else {
-      setSeconds(seconds);
-      setMinutes(minutes);
-      setHours(hours);
-      setDays(days);
-    }
-  }
-
+export default function deprecatedUseTimer(settings) {
   // didMount effect
   useEffect(() => {
-    start();
-    return stopTimer;
+    console.warn('react-timer-hook: default export useTimer is deprecated, use named exports { useTimer, useStopwatch } instead');
   },[]);
 
-
-  // Validate expiryTimestamp
-  function isValidExpiryTimestamp(expiryTimestamp) {
-    const isValid = expiryTimestamp && (new Date(expiryTimestamp)).getTime() > 0;
-    if(expiryTimestamp && !isValid) {
-      console.warn('react-timer-hook: Invalid expiryTimestamp settings passed', expiryTimestamp);
-    }
-    return isValid;
+  if(settings.expiryTimestamp) {
+    return useTimer(settings);
+  } else {
+    return useStopwatch(settings);
   }
-
-  // Validate onExpire
-  function isValidOnExpire(onExpire) {
-    const isValid = onExpire && typeof onExpire === 'function';
-    if(onExpire && !isValid) {
-      console.warn('react-timer-hook: Invalid onExpire settings function passed', onExpire);
-    }
-    return isValid;
-  }
-
-  return { seconds, minutes, hours, days, startTimer, stopTimer, resetTimer };
 }
 
-
 /* --------------------- useStopwatch ----------------------- */
-
 
 export function useStopwatch(settings) {
   const { autoStart } = settings || {};
@@ -226,8 +98,6 @@ export function useStopwatch(settings) {
   return { seconds, minutes, hours, days, start, pause, reset };
 }
 
-
-
 /* ---------------------- useTimer --------------------- */
 
 export function useTimer(settings) {
@@ -240,15 +110,11 @@ export function useTimer(settings) {
 
   const intervalRef = useRef();
 
-  function runCountdownTimer() {
-    if(!intervalRef.current) {
+  function start() {
+    if(isValidExpiryTimestamp(expiryTimestamp) && !intervalRef.current) {
       calculateExpiryDate();
       intervalRef.current = setInterval(() => calculateExpiryDate(), 1000);
     }
-  }
-
-  function start() {
-    isValidExpiryTimestamp(expiryTimestamp) && runCountdownTimer();
   }
 
   function pause() {
