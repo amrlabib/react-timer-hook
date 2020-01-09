ecd6cc3329de2c7aa1a4815d4b77dcf70ded67f0
Amr Labib <amaly@noon.com>
2020-01-09 17:52:13 +0400

    lint src/index.js for react hooks

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import useTimer from './useTimer';
 import useStopwatch from './useStopwatch';
 import useTime from './useTime';
 
+
 export {
   useTimer,
   useStopwatch,
   useTime,
 };
+
 
 // This deprecated default export is just to avoid breaking old versions code before v1.1.0
 export default function useTimerDeprecated(settings) {
@@ -16,14 +18,20 @@ export default function useTimerDeprecated(settings) {
     console.warn('react-timer-hook: default export useTimer is deprecated, use named exports { useTimer, useStopwatch, useTime } instead'); // eslint-disable-line
   }, []);
 
+  /*
+    We disable react hook condition lint error here, because older versions can either
+    pass useTimer or useStopwatch settings, this means this condition is static
+    for any specific component
+  */
+
   if (settings.expiryTimestamp) {
-    const values = useTimer(settings);
+    const values = useTimer(settings); // eslint-disable-line
     return {
       ...values, startTimer: values.start, stopTimer: values.pause, resetTimer: () => {},
     };
   }
 
-  const values = useStopwatch(settings);
+  const values = useStopwatch(settings); // eslint-disable-line
   return {
     ...values, startTimer: values.start, stopTimer: values.pause, resetTimer: values.reset,
   };
