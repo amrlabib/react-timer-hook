7c627db84d9ca91a0d5f0e1a1c7dd9e42111ce17
Amr Labib <amr.h.labib@gmail.com>
2020-01-10 00:46:49 +0400

    fix useTimer lint for react hooks

--- src/useTime.js
+++ src/useTime.js
@@ -8,14 +8,7 @@ export default function useTime(settings) {
   const [minutes, setMinutes] = useState(0);
   const [hours, setHours] = useState(0);
   const [ampm, setAmPm] = useState('');
-
   const intervalRef = useRef();
-  function start() {
-    if (!intervalRef.current) {
-      setCurrentTime();
-      intervalRef.current = setInterval(() => setCurrentTime(), 1000);
-    }
-  }
 
   function reset() {
     if (intervalRef.current) {
@@ -28,35 +21,41 @@ export default function useTime(settings) {
     setAmPm('');
   }
 
-  function formatHours(hoursValue) {
-    if (format === '12-hour') {
-      const ampmValue = hours >= 12 ? 'pm' : 'am';
-      let formattedHours = hoursValue % 12;
-      formattedHours = formattedHours || 12;
-      return { hoursValue: formattedHours, ampmValue };
-    }
-    return { hoursValue, ampmValue: '' };
-  }
-
-
-  function setCurrentTime() {
-    const now = new Date();
-    const second = now.getSeconds();
-    const minute = now.getMinutes();
-    const { hoursValue, ampmValue } = formatHours(now.getHours());
-
-
-    setSeconds(second);
-    setMinutes(minute);
-    setHours(hoursValue);
-    setAmPm(ampmValue);
-  }
-
   // didMount effect
   useEffect(() => {
+    function formatHours(hoursValue) {
+      if (format === '12-hour') {
+        const ampmValue = hoursValue >= 12 ? 'pm' : 'am';
+        let formattedHours = hoursValue % 12;
+        formattedHours = formattedHours || 12;
+        return { hoursValue: formattedHours, ampmValue };
+      }
+      return { hoursValue, ampmValue: '' };
+    }
+
+    function setCurrentTime() {
+      const now = new Date();
+      const secondsValue = now.getSeconds();
+      const minutesValue = now.getMinutes();
+      const { hoursValue, ampmValue } = formatHours(now.getHours());
+
+
+      setSeconds(secondsValue);
+      setMinutes(minutesValue);
+      setHours(hoursValue);
+      setAmPm(ampmValue);
+    }
+
+    function start() {
+      if (!intervalRef.current) {
+        setCurrentTime();
+        intervalRef.current = setInterval(() => setCurrentTime(), 1000);
+      }
+    }
+
     start();
     return reset;
-  }, []);
+  }, [format]);
 
 
   return {
