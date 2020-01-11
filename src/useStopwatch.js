0c4106004456224bf777938ac75f0bc7f1d953f7
Amr Labib <amr.h.labib@gmail.com>
2020-01-11 19:06:28 +0400

    use distance state in useStopwatch

--- src/useStopwatch.js
+++ src/useStopwatch.js
@@ -1,62 +1,26 @@
 import { useState, useEffect, useRef } from 'react';
+import { Time } from './utils';
 
 /* --------------------- useStopwatch ----------------------- */
 
 export default function useStopwatch(settings) {
   const { autoStart } = settings || {};
 
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [timeDistance, setTimeDistance] = useState(0);
   const [isInitialStart, setIsInitialStart] = useState(true);
   const intervalRef = useRef();
-
-  function addDay() {
-    setDays((prevDays) => (prevDays + 1));
-  }
-
-  function addHour() {
-    setHours((prevHours) => {
-      if (prevHours === 23) {
-        addDay();
-        return 0;
-      }
-      return prevHours + 1;
-    });
-  }
-
-  function addMinute() {
-    setMinutes((prevMinutes) => {
-      if (prevMinutes === 59) {
-        addHour();
-        return 0;
-      }
-      return prevMinutes + 1;
-    });
-  }
-
-  function addSecond() {
-    setSeconds((prevSeconds) => {
-      if (prevSeconds === 59) {
-        addMinute();
-        return 0;
-      }
-      return prevSeconds + 1;
-    });
-  }
-
-  function start() {
-    setIsInitialStart(false);
-    if (!intervalRef.current) {
-      intervalRef.current = setInterval(() => addSecond(), 1000);
-    }
-  }
 
   function clearIntervalRef() {
     if (intervalRef.current) {
       clearInterval(intervalRef.current);
       intervalRef.current = undefined;
+    }
+  }
+
+  function start() {
+    setIsInitialStart(false);
+    if (!intervalRef.current) {
+      intervalRef.current = setInterval(() => setTimeDistance((prevDistance) => prevDistance + 1001), 1000);
     }
   }
 
@@ -67,10 +31,7 @@ export default function useStopwatch(settings) {
   function reset() {
     setIsInitialStart(true);
     clearIntervalRef();
-    setSeconds(0);
-    setMinutes(0);
-    setHours(0);
-    setDays(0);
+    setTimeDistance(0);
   }
 
 
@@ -83,6 +44,6 @@ export default function useStopwatch(settings) {
   });
 
   return {
-    seconds, minutes, hours, days, start, pause, reset,
+    ...Time.getTimeForDistance(timeDistance), start, pause, reset,
   };
 }
