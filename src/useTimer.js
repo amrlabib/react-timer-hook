d827321d12633fb133768f34db6b65f6186160eb
Amr Labib <amr.h.labib@gmail.com>
2020-01-13 20:40:05 +0400

    lint useTimer

--- src/useTimer.js
+++ src/useTimer.js
@@ -1,78 +1,33 @@
 import { useState, useEffect, useRef } from 'react';
 
-/* ---------------------- useTimer --------------------- */
+
+// Validate expiryTimestamp
+function isValidExpiryTimestamp(expiryTimestamp) {
+  const isValid = (new Date(expiryTimestamp)).getTime() > 0;
+  if (!isValid) {
+    console.warn('react-timer-hook: { useTimer } Invalid expiryTimestamp settings', expiryTimestamp);
+  }
+  return isValid;
+}
+
+// Validate onExpire
+function isValidOnExpire(onExpire) {
+  const isValid = onExpire && typeof onExpire === 'function';
+  if (onExpire && !isValid) {
+    console.warn('react-timer-hook: { useTimer } Invalid onExpire settings function', onExpire);
+  }
+  return isValid;
+}
 
 export default function useTimer(settings) {
   const { expiryTimestamp: expiry, onExpire } = settings || {};
   const [expiryTimestamp, setExpiryTimestamp] = useState(expiry);
 
   const [seconds, setSeconds] = useState(0);
-  function subtractSecond() {
-    setSeconds(prevSeconds => {
-      if(prevSeconds === 0) {
-        subtractMinute();
-        return 59;
-      } else if(prevSeconds > 0) {
-        return prevSeconds - 1;
-      }
-      return 0;
-    });
-  }
-
-
   const [minutes, setMinutes] = useState(0);
-  function subtractMinute() {
-    setMinutes(prevMinutes => {
-      if (prevMinutes === 0) {
-        subtractHour();
-        return 59;
-      } else if(prevMinutes > 0) {
-        return prevMinutes - 1;
-      }
-      return 0;
-    });
-  }
-
   const [hours, setHours] = useState(0);
-  function subtractHour() {
-    setHours(prevHours => {
-      if (prevHours === 0) {
-        subtractDay();
-        return 23;
-      } else if(prevHours > 0) {
-        return prevHours - 1;
-      }
-      return 0;
-    });
-  }
-
   const [days, setDays] = useState(0);
-  function subtractDay() {
-    setDays(prevDays => {
-      if(prevDays > 0) {
-        return prevDays - 1;
-      }
-      reset();
-      isValidOnExpire(onExpire) && onExpire();
-      return 0;
-    });
-  }
-
   const intervalRef = useRef();
-
-  function start() {
-    if(isValidExpiryTimestamp(expiryTimestamp) && !intervalRef.current) {
-      calculateExpiryDate();
-      intervalRef.current = setInterval(() => calculateExpiryDate(), 1000);
-    }
-  }
-
-  function pause() {
-    if(intervalRef.current) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = undefined;
-    }
-  }
 
   function reset() {
     if (intervalRef.current) {
@@ -85,8 +40,94 @@ export default function useTimer(settings) {
     setDays(0);
   }
 
+  function subtractDay() {
+    setDays((prevDays) => {
+      if (prevDays > 0) {
+        return prevDays - 1;
+      }
+      reset();
+      isValidOnExpire(onExpire) && onExpire();
+      return 0;
+    });
+  }
+
+  function subtractHour() {
+    setHours((prevHours) => {
+      if (prevHours === 0) {
+        subtractDay();
+        return 23;
+      }
+
+      if (prevHours > 0) {
+        return prevHours - 1;
+      }
+      return 0;
+    });
+  }
+
+  function subtractMinute() {
+    setMinutes((prevMinutes) => {
+      if (prevMinutes === 0) {
+        subtractHour();
+        return 59;
+      }
+
+      if (prevMinutes > 0) {
+        return prevMinutes - 1;
+      }
+      return 0;
+    });
+  }
+
+  function subtractSecond() {
+    setSeconds((prevSeconds) => {
+      if (prevSeconds === 0) {
+        subtractMinute();
+        return 59;
+      }
+
+      if (prevSeconds > 0) {
+        return prevSeconds - 1;
+      }
+      return 0;
+    });
+  }
+
+  // Timer expiry date calculation
+  function calculateExpiryDate() {
+    const now = new Date().getTime();
+    const distance = expiryTimestamp - now;
+    const daysValue = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hoursValue = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutesValue = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsValue = Math.floor((distance % (1000 * 60)) / 1000);
+    if (seconds < 0) {
+      reset();
+      isValidOnExpire(onExpire) && onExpire();
+    } else {
+      setSeconds(secondsValue);
+      setMinutes(minutesValue);
+      setHours(hoursValue);
+      setDays(daysValue);
+    }
+  }
+
+  function start() {
+    if (isValidExpiryTimestamp(expiryTimestamp) && !intervalRef.current) {
+      calculateExpiryDate();
+      intervalRef.current = setInterval(() => calculateExpiryDate(), 1000);
+    }
+  }
+
+  function pause() {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = undefined;
+    }
+  }
+
   function resume() {
-    if(isValidExpiryTimestamp(expiryTimestamp) && !intervalRef.current) {
+    if (isValidExpiryTimestamp(expiryTimestamp) && !intervalRef.current) {
       intervalRef.current = setInterval(() => subtractSecond(), 1000);
     }
   }
@@ -96,50 +137,14 @@ export default function useTimer(settings) {
     setExpiryTimestamp(newExpiryTimestamp);
   }
 
-
-  // Timer expiry date calculation
-  function calculateExpiryDate() {
-    var now = new Date().getTime();
-    var distance = expiryTimestamp - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    if(seconds < 0) {
-      reset();
-      isValidOnExpire(onExpire) && onExpire();
-    } else {
-      setSeconds(seconds);
-      setMinutes(minutes);
-      setHours(hours);
-      setDays(days);
-    }
-  }
-
   // didMount effect
   useEffect(() => {
     start();
     return reset;
-  },[expiryTimestamp]);
+  }, [expiryTimestamp]);
 
 
-  // Validate expiryTimestamp
-  function isValidExpiryTimestamp(expiryTimestamp) {
-    const isValid = (new Date(expiryTimestamp)).getTime() > 0;
-    if(!isValid) {
-      console.warn('react-timer-hook: { useTimer } Invalid expiryTimestamp settings', expiryTimestamp);
-    }
-    return isValid;
-  }
-
-  // Validate onExpire
-  function isValidOnExpire(onExpire) {
-    const isValid = onExpire && typeof onExpire === 'function';
-    if(onExpire && !isValid) {
-      console.warn('react-timer-hook: { useTimer } Invalid onExpire settings function', onExpire);
-    }
-    return isValid;
-  }
-
-  return { seconds, minutes, hours, days, start, pause, resume, restart };
+  return {
+    seconds, minutes, hours, days, start, pause, resume, restart,
+  };
 }
