0dae45f19183a4f380688c545bdbad1342055977
Amr Labib <amr.h.labib@gmail.com>
2021-04-11 02:49:47 +0400

    add useInterval in useStopwatch

--- src/useStopwatch.js
+++ src/useStopwatch.js
@@ -1,47 +1,27 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import { Time } from './utils';
+import { useInterval } from './hooks';
 
-export default function useStopwatch(settings) {
-  const { autoStart, offsetTimestamp } = settings || {};
-
+export default function useStopwatch({ autoStart, offsetTimestamp }) {
   const [seconds, setSeconds] = useState(Time.getSecondsFromExpiry(offsetTimestamp || 0));
   const [isRunning, setIsRunning] = useState(autoStart);
-  const intervalRef = useRef();
 
-  function clearIntervalRef() {
-    if (intervalRef.current) {
-      setIsRunning(false);
-      clearInterval(intervalRef.current);
-      intervalRef.current = undefined;
-    }
-  }
+  useInterval(isRunning ? () => {
+    setSeconds((prevSeconds) => (prevSeconds + 1));
+  } : () => {}, 1000);
 
   function start() {
-    if (!intervalRef.current) {
-      setIsRunning(true);
-      intervalRef.current = setInterval(() => setSeconds((prevSeconds) => (prevSeconds + 1)), 1000);
-    }
+    setIsRunning(true);
   }
 
   function pause() {
-    clearIntervalRef();
+    setIsRunning(false);
   }
 
   function reset(offset: number) {
-    clearIntervalRef();
+    setIsRunning(autoStart);
     setSeconds(Time.getSecondsFromExpiry(offset || 0));
-    if (autoStart) {
-      start();
-    }
   }
-
-  // didMount effect
-  useEffect(() => {
-    if (autoStart) {
-      start();
-    }
-    return clearIntervalRef;
-  }, []);
 
   return {
     ...Time.getTimeFromSeconds(seconds), start, pause, reset, isRunning,
