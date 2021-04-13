8dd11b0020f65cf0b6650ccf808feca15cf2e777
Amr Labib <amr.h.labib@gmail.com>
2021-04-14 02:29:38 +0400

    handle milliseconds in useTimer after new implementation

--- src/useTimer.js
+++ src/useTimer.js
@@ -2,52 +2,60 @@ import { useState } from 'react';
 import { Time, Validate } from './utils';
 import { useInterval } from './hooks';
 
+const DEFAULT_DELAY = 1000;
 export default function useTimer({ expiryTimestamp: expiry, onExpire, autoStart }) {
   const [expiryTimestamp, setExpiryTimestamp] = useState(expiry);
   const [seconds, setSeconds] = useState(Time.getSecondsFromExpiry(expiryTimestamp));
   const [isRunning, setIsRunning] = useState(autoStart);
   const [didStart, setDidStart] = useState(autoStart);
+  const extraMilliSeconds = Math.floor((seconds - Math.floor(seconds)) * 1000);
+  const [delay, setDelay] = useState(extraMilliSeconds > 0 ? extraMilliSeconds : 1000);
 
   function handleExpire() {
     Validate.onExpire(onExpire) && onExpire();
+    setIsRunning(false);
+    setDelay(null);
   }
 
   function pause() {
     setIsRunning(false);
   }
 
+  function restart(newExpiryTimestamp, newAutoStart) {
+    const secondsValue = Time.getSecondsFromExpiry(newExpiryTimestamp);
+    const extraMilliSecondsValue = Math.floor((secondsValue - Math.floor(secondsValue)) * 1000);
+    setDelay(extraMilliSecondsValue > 0 ? extraMilliSecondsValue : 1000);
+    setDidStart(newAutoStart);
+    setIsRunning(newAutoStart);
+    setExpiryTimestamp(newExpiryTimestamp);
+    setSeconds(secondsValue);
+  }
+
   function resume() {
     const time = new Date();
-    time.setSeconds(time.getSeconds() + seconds); // calculate new expiry timestamp based on last paused seconds count
-    setExpiryTimestamp(time);
-    setIsRunning(true);
-    setSeconds(Time.getSecondsFromExpiry(time));
+    time.setMilliseconds(time.getMilliseconds() + (seconds * 1000));
+    restart(time, true);
   }
 
   function start() {
     if (didStart) {
-      setIsRunning(true);
       setSeconds(Time.getSecondsFromExpiry(expiryTimestamp));
+      setIsRunning(true);
     } else {
       resume();
-      setDidStart(true);
     }
-  }
-
-  function restart(newExpiryTimestamp) {
-    setDidStart(autoStart);
-    setIsRunning(autoStart);
-    setExpiryTimestamp(newExpiryTimestamp);
-    setSeconds(Time.getSecondsFromExpiry(newExpiryTimestamp));
   }
 
   useInterval(() => {
+    if (delay !== DEFAULT_DELAY) {
+      setDelay(DEFAULT_DELAY);
+    }
     const secondsValue = Time.getSecondsFromExpiry(expiryTimestamp);
+    setSeconds(secondsValue);
     if (secondsValue <= 0) {
       handleExpire();
     }
-    setSeconds(secondsValue);
-  }, isRunning ? 1000 : null);
+  }, isRunning ? delay : null);
 
   return {
     ...Time.getTimeFromSeconds(seconds), start, pause, resume, restart, isRunning,
