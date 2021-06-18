0c648c126f36606e55ca58c8197a84177f879eb1
Amr Labib <amr.h.labib@gmail.com>
2021-06-18 17:52:44 +0200

    fix useStopwatch

--- src/useStopwatch.js
+++ src/useStopwatch.js
@@ -3,24 +3,34 @@ import { Time } from './utils';
 import { useInterval } from './hooks';
 
 export default function useStopwatch({ autoStart, offsetTimestamp }) {
-  const [seconds, setSeconds] = useState(Time.getSecondsFromExpiry(offsetTimestamp || 0));
+  const [passedSeconds, setPassedSeconds] = useState(Time.getSecondsFromExpiry(offsetTimestamp, true) || 0);
+  const [prevTime, setPrevTime] = useState(new Date());
+  const [seconds, setSeconds] = useState(passedSeconds + Time.getSecondsFromPrevTime(prevTime || 0, true));
   const [isRunning, setIsRunning] = useState(autoStart);
 
   useInterval(() => {
-    setSeconds((prevSeconds) => (prevSeconds + 1));
+    setSeconds(passedSeconds + Time.getSecondsFromPrevTime(prevTime, true));
   }, isRunning ? 1000 : null);
 
   function start() {
+    const newPrevTime = new Date();
+    setPrevTime(newPrevTime);
     setIsRunning(true);
+    setSeconds(passedSeconds + Time.getSecondsFromPrevTime(newPrevTime, true));
   }
 
   function pause() {
+    setPassedSeconds(seconds);
     setIsRunning(false);
   }
 
   function reset(offset = 0, newAutoStart = true) {
+    const newPassedSeconds = Time.getSecondsFromExpiry(offset, true) || 0;
+    const newPrevTime = new Date();
+    setPrevTime(newPrevTime);
+    setPassedSeconds(newPassedSeconds);
     setIsRunning(newAutoStart);
-    setSeconds(Time.getSecondsFromExpiry(offset));
+    setSeconds(newPassedSeconds + Time.getSecondsFromPrevTime(newPrevTime, true));
   }
 
   return {
